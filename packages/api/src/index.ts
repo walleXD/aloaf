a8a0d1b6a5a5b888218157dfc9c5f273d8c53877
Walee Ahmed <dev@walee.me>
2019-07-24 00:39:48 -0400

    removed backpack from api build pipe

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,2 +1,24 @@
-const msg: string = 'Hello'
-console.log(msg)
+import { ApolloServer, gql } from 'apollo-server-micro'
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (root, args, context) => {
+      return "Hello world! It's your boy, how far now unicodeveloper"
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+})
+
+module.exports = server.createHandler()
