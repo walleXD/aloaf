aa86f5c7197c691ecba1c437405d273246ddce49
Walee Ahmed <dev@walee.me>
2019-08-08 17:35:14 -0400

    added tsc to build all packages

--- packages/web/src/modules/landing/index.tsx
+++ packages/web/src/modules/landing/index.tsx
@@ -2,19 +2,23 @@ import React, { ReactElement } from 'react'
 import { Typography } from '@material-ui/core'
 import { HelloWorldComponent } from '../../generated/GraphQLComponents'
 
+import { Layout } from '@loaf/web-common'
+
 const IndexPage = (): ReactElement => (
-  <HelloWorldComponent>
-    {({ loading, error, data }): ReactElement => {
-      if (loading) return <div>Loading...</div>
-      else if (error) return <div>Error :(</div>
-      else
-        return data ? (
-          <Typography>{data.hello}</Typography>
-        ) : (
-          <div>Error :(</div>
-        )
-    }}
-  </HelloWorldComponent>
+  <Layout title="Home">
+    <HelloWorldComponent>
+      {({ loading, error, data }): ReactElement => {
+        if (loading) return <div>Loading...</div>
+        else if (error) return <div>Error :(</div>
+        else
+          return data ? (
+            <Typography>{data.hello}</Typography>
+          ) : (
+            <div>Error :(</div>
+          )
+      }}
+    </HelloWorldComponent>
+  </Layout>
 )
 
 export default IndexPage
