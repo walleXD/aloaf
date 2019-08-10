b00df7888c8a50c94e6b0588e85804ff240135c1
Walee Ahmed <dev@walee.me>
2019-08-09 22:49:13 -0400

    updated deps

--- packages/web/src/next.config.js
+++ packages/web/src/next.config.js
@@ -1,8 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withPlugins = require('next-compose-plugins')
 const withTM = require('next-transpile-modules')
+const nextEnv = require('next-env')
+const dotenvLoad = require('dotenv-load')
+
+dotenvLoad()
 
 const plugins = [
+  nextEnv(),
   withTM({
     transpileModules: ['@loaf/web-auth', '@loaf/web-common']
   })
